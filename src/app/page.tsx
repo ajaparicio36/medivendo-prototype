c1b0f797bf39347569e4b505e9187109c6ac52ad
AJ Aparicio <anthonyjohn.aparicio-22@cpu.edu.ph>
2024-11-22 23:41:41 +0800

    refactor: increase home page size

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,21 +11,23 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <div className="min-h-screen flex items-center justify-center bg-white">
-      <Card className="w-[350px]">
-        <CardHeader>
-          <CardTitle className="text-black text-center">Welcome</CardTitle>
-          <CardDescription className="text-center">
+      <Card className="w-[450px]">
+        <CardHeader className="space-y-2">
+          <CardTitle className="text-black text-center text-3xl">
+            Welcome
+          </CardTitle>
+          <CardDescription className="text-center text-lg">
             Choose an option to proceed
           </CardDescription>
         </CardHeader>
-        <CardContent className="flex flex-col space-y-4">
+        <CardContent className="flex flex-col space-y-6 p-6">
           <Link href="/chat" passHref>
-            <Button className="w-full bg-[#2f27ce] hover:bg-[#2f27ce]/90">
+            <Button className="w-full bg-[#2f27ce] hover:bg-[#2f27ce]/90 text-lg py-6">
               Chat with AI
             </Button>
           </Link>
           <Link href="/catalogue" passHref>
-            <Button className="w-full bg-[#dedcff] text-black hover:bg-[#dedcff]/90">
+            <Button className="w-full bg-[#dedcff] text-black hover:bg-[#dedcff]/90 text-lg py-6">
               Buy Medicine
             </Button>
           </Link>
